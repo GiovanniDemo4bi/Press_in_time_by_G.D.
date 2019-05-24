0d5c9c1fd4f464ceed3ace0ddd8489172db42bdd
GiovanniDemo4bi <46590543+GiovanniDemo4bi@users.noreply.github.com>
2019-05-24 23:40:52 +0200

    Add files via upload
    Programma non completato,funzionamento di molte funzioni tranne quella che riguarda la pressione dei bottoni

--- Press_in_time_by_G.D/sketch_may14a/sketch_may14a.ino
+++ Press_in_time_by_G.D/sketch_may14a/sketch_may14a.ino
@@ -2,17 +2,19 @@
 #define RIGHT 10
 #define CENTER 9
 #define LEFT 8
-bool correct = false;
-int heartsToTakeAway = 0;
-int heartsToAdd = 0;
-int i = 10;
+int buttons[] = {RIGHT,CENTER,LEFT};
+int randomPosition;
 int positionX = 0;
 int theresOrNot = -1;
 bool smilingFace;
-bool angryFace;
+bool skull;
 bool hearth;
-int nHealth;
+int nHealth = 3;
 int nPoint;
+int y = 15;
+int x = 1000;
+float startGame;
+float endGame;
 
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
@@ -24,7 +26,7 @@ byte halfheartleft[8] = {
   B01111,
   B00111,
   B00011,
-  B00001
+  B00001,
 };
 
 byte halfheartright[8] = {
@@ -35,7 +37,7 @@ byte halfheartright[8] = {
   B11110,
   B11100,
   B11000,
-  B10000
+  B10000,
 };
 
 byte halfsmilefaceright[8] = {
@@ -46,7 +48,7 @@ byte halfsmilefaceright[8] = {
   B00001,
   B00010,
   B11100,
-  B00000
+  B00000,
 };
 
 byte halfsmilefaceleft[8] = {
@@ -57,29 +59,7 @@ byte halfsmilefaceleft[8] = {
   B10000,
   B01000,
   B00111,
-  B00000
-};
-
-byte halfangryfaceleft[8] = {
-  B10000,
-  B01110,
-  B00110,
-  B00110,
   B00000,
-  B00111,
-  B01000,
-  B00000
-};
-
-byte halfangryfaceright[8] = {
-  B00001,
-  B01110,
-  B01100,
-  B01100,
-  B00000,
-  B11100,
-  B00010,
-  B00000
 };
 
 byte halfskullleft[8] = {
@@ -90,7 +70,7 @@ byte halfskullleft[8] = {
   B01110,
   B01110,
   B00111,
-  B00111
+  B00101,
 };
 
 byte halfskullright[8] = {
@@ -101,7 +81,7 @@ byte halfskullright[8] = {
   B01110,
   B01110,
   B11100,
-  B11100
+  B10100
 };
 
 void setup() 
@@ -115,120 +95,277 @@ void setup()
   lcd.createChar(1, halfheartright);
   lcd.createChar(2, halfsmilefaceright);
   lcd.createChar(3, halfsmilefaceleft);
-  lcd.createChar(4, halfangryfaceleft);
-  lcd.createChar(5, halfangryfaceright);
-  lcd.createChar(6, halfskullleft);
-  lcd.createChar(7, halfskullright);
+  lcd.createChar(4, halfskullleft);
+  lcd.createChar(5, halfskullright);
   randomSeed(analogRead(0)); 
 }
 
 void loop() 
 {
- full_health();
-}
-
-void full_hearth(int x)
-{
-  lcd.setCursor(x,0);
-  lcd.write(byte(1));
-  lcd.setCursor(x-1,0);
-  lcd.write(byte(0));
-}
-
-void full_health()
-{
-  for(int i = 16; i<10; i-2)
+  introduction();
+  set_level();
+  give_health(nHealth);
+  while(nHealth != 0)
   {
-    nHealth = 3; 
-    full_hearth(i);
-    delay(500);
+    press_in_time();
+  }
+  game_over();
+}
+
+void introduction()
+{
+  lcd.clear();
+  lcd.setCursor(1,0);
+  lcd.print("Press in time!");
+  lcd.setCursor(5,1);
+  lcd.print("by G.D.");
+  delay(2000);
+}
+
+void give_health(int nHearth)
+{
+  lcd.clear();
+  lcd.setCursor(7,0);
+  lcd.print("HP:");
+  int x = 15;
+  int type = 1;
+  for (int i = 0; i < (nHearth*2); i++)
+  {
+    lcd.setCursor(x,0);
+    lcd.write(byte(type));
+    x--;
+    if(type == 1)
+    {
+      type = 0;
+    }
+    else
+    {
+      type = 1;
+    }
   }
 }
 
-void remove_health(int hearts)
+void set_level()
 {
-  int nHearth = 0;
-  while(nHearth = hearts)
+  bool choose = false;
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("EASY NORMAL HARD");
+  while(!choose)
   {
-    lcd.setCursor(i,0);
+  if(digitalRead(LEFT) == LOW)
+  {
+    choose = true;
+    x = 1000;
     lcd.clear();
-    lcd.setCursor(i-1,0);
+    lcd.setCursor(3,0);
+    lcd.print("EASY MODE!");
+    delay(1000);
     lcd.clear();
-    delay(500);
-    nHearth++;
-    i+2;
+    lcd.setCursor(6,0);
+    lcd.print("3...");
+    delay(1000);
+    lcd.setCursor(6,0);
+    lcd.print("2...");
+    delay(1000);
+    lcd.setCursor(6,0);
+    lcd.print("1...");
+    delay(1000);
+  }
+  if(digitalRead(CENTER) == LOW)
+  {
+    choose = true;
+    x = 750;
+    lcd.clear();
+    lcd.setCursor(2,1);
+    lcd.print("NORMAL MODE!");
+    delay(1000);
+    lcd.clear();
+    lcd.setCursor(6,0);
+    lcd.print("3...");
+    delay(1000);
+    lcd.setCursor(6,0);
+    lcd.print("2...");
+    delay(1000);
+    lcd.setCursor(6,0);
+    lcd.print("1...");
+    delay(1000);
+  }
+  if(digitalRead(RIGHT) == LOW)
+  {
+    choose = true;
+    x = 500;
+    lcd.clear();
+    lcd.setCursor(3,1);
+    lcd.print("HARD MODE!");
+    delay(1000);
+    lcd.clear();
+    lcd.setCursor(6,0);
+    lcd.print("3...");
+    delay(1000);
+    lcd.setCursor(6,0);
+    lcd.print("2...");
+    delay(1000);
+    lcd.setCursor(6,0);
+    lcd.print("1...");
+    delay(1000);
+  }
   }
 }
-
-void add_health(int hearts)
+void  press_in_time()
 {
-  int nHearth = 0;
-  while(nHearth = hearts)
+  random_position();
+  if(digitalRead(RIGHT) == LOW || digitalRead(CENTER) == LOW || digitalRead(LEFT) == LOW)
   {
-    lcd.setCursor(i-1,0);
-    lcd.write(byte(1));
-    lcd.setCursor(i-2,0);
-    lcd.write(byte(0));
-    delay(500);
-    nHearth++;
-    i-2;
+    if (nHealth > 0 && theresOrNot == 0)
+    {
+      nHealth--;
+      give_health(nHealth);
+      nPoint -= 10;
+      give_points();
+    }
   }
-}
-
-void add_or_remove_health(bool correct)
-{
-  if(correct = false)
+  else if((digitalRead(LEFT) == LOW) && (theresOrNot == 1) && (positionX == 1))
   {
-    nHealth --;
-    heartsToTakeAway++;
-    remove_health(heartsToTakeAway);
+    if(smilingFace = true)
+    {
+    if (nHealth > 0)
+    {
+      nHealth--;
+      give_health(nHealth);
+      nPoint -= 10;
+      give_points();
+    }
+    }
+    else if(hearth = true)
+    {
+      if (nHealth < 3)
+      {
+        nHealth++;
+        give_health(nHealth);
+      }
+    }
+    else if(skull = true)
+    {
+      nPoint += 10;
+      give_points();
+    }
   }
-  else if (correct = true)
+    else if((digitalRead(CENTER) == LOW) && (theresOrNot = 1) && (positionX = 4))
   {
-    nHealth ++;
-    heartsToAdd++;
-    add_health(heartsToAdd);
+    if(smilingFace = true)
+    {   
+    if (nHealth > 0)
+    {
+      nHealth--;
+      give_health(nHealth);
+      nPoint -= 10;
+      give_points();
+    }
+    }
+    else if(hearth = true)
+    {
+      if (nHealth < 3)
+      {
+        nHealth++;
+        give_health(nHealth);
+      }
+    }
+    else if(skull = true)
+    {
+       nPoint += 10;
+       give_points();
+    }
+  }
+      else if((digitalRead(RIGHT) == LOW) && (theresOrNot = 1) && (positionX = 7))
+  {
+    if(smilingFace = true)
+    {
+    if (nHealth > 0)
+    {
+      nHealth--;
+      give_health(nHealth);
+      nPoint -= 10;
+      give_points();
+    }
+    }
+    else if(hearth = true)
+    {
+      if (nHealth < 3)
+      {
+        nHealth++;
+        give_health(nHealth);
+      }
+    }
+    else if(skull = true)
+    {
+      nPoint += 10;
+      give_points();
+    }
   }
 }
 
 void random_char(int positionChar)
 {
   int randomChar = random(0,101);
+  lcd.setCursor(0,1);
+  lcd.print("|");
+  lcd.setCursor(3,1);
+  lcd.print("|");
+  lcd.setCursor(6,1);
+  lcd.print("|");
+  lcd.setCursor(9,1);
+  lcd.print("|");
   if((randomChar >= 0) && (randomChar <= 20))
   {
     bool smilingFace = false;
-    bool angryFace = false;
+    bool skull = false;
     bool hearth = true;
     lcd.setCursor(positionChar,1);
     lcd.write(byte(0));
     lcd.setCursor(positionChar+1,1);
     lcd.write(byte(1));
+    delay(x);
+    lcd.setCursor(positionChar,1);
+    lcd.print(" ");
+    lcd.setCursor(positionChar+1,1);
+    lcd.print(" ");
   }
   else if((randomChar > 20) && (randomChar <= 50))
   {
     bool smilingFace = true;
-    bool angryFace = false;
+    bool skull = false;
     bool hearth = false;
     lcd.setCursor(positionChar,1);
     lcd.write(byte(3));
     lcd.setCursor(positionChar+1,1);
     lcd.write(byte(2));
+    delay(x);
+     lcd.setCursor(positionChar,1);
+    lcd.print(" ");
+    lcd.setCursor(positionChar+1,1);
+    lcd.print(" ");
   }
   else if((randomChar > 50) && (randomChar <= 100))
   {
     bool smilingFace = false;
-    bool angryFace = true;
+    bool skull = true;
     bool hearth = false;
     lcd.setCursor(positionChar,1);
     lcd.write(byte(4));
     lcd.setCursor(positionChar+1,1);
     lcd.write(byte(5));
+    delay(x);
+    lcd.setCursor(positionChar,1);
+    lcd.print(" ");
+    lcd.setCursor(positionChar+1,1);
+    lcd.print(" ");
   }
 }
 
 void random_position()
 {
-  int randomPosition = random(0,3);
+  randomPosition = random(0,3);
   theresOrNot = 0;
    if(randomPosition == 0)
   {
@@ -250,90 +387,29 @@ void random_position()
   }
 }
 
-void  press_in_time()
+void give_points()
 {
-  if(digitalRead(RIGHT) == LOW || digitalRead(CENTER) == LOW || digitalRead(LEFT) == LOW && theresOrNot = 0)
-  {
-    if (nHealth > 0)
-    {
-      add_or_remove_health(false);
-    }
-    else{}
-    nPoint -= 10;
-  }
-  else if((theresOrNot = 1) && (positionX = 1) && (digitalRead(LEFT) == LOW))
-  {
-    if(smilingFace = true)
-    {
-    if (nHealth > 0)
-    {
-      add_or_remove_health(false);
-    }
-    else{}
-    nPoint -= 10;
-    }
-    else if(hearth = true)
-    {
-      if (nHealth < 3)
-      {
-        add_or_remove_health(true);
-      }
-      else{}
-    }
-    else if(angryFace = true)
-    {
-      nPoint += 10;
-    }
-  }
-    else if((theresOrNot = 1) && (positionX = 4) && (digitalRead(CENTER) == LOW))
-  {
-    if(smilingFace = true)
-    {   
-    if (nHealth > 0)
-    {
-      add_or_remove_health(false);
-    }
-    else{}
-    nPoint -= 10;
-    }
-    else if(hearth = true)
-    {
-      if (nHealth < 3)
-      {
-        add_or_remove_health(true);
-      }
-      else
-      {}
-    }
-    else if(angryFace = true)
-    {
-       nPoint += 10;
-    }
-  }
-      else if((theresOrNot = 1) && (positionX = 7) && (digitalRead(RIGHT) == LOW))
-  {
-    if(smilingFace = true)
-    {
-    if (nHealth > 0)
-    {
-      add_or_remove_health(false);
-    }
-    else{}
-    nPoint -= 10;
-    }
-    else if(hearth = true)
-    {
-      if (nHealth < 3)
-      {
-        add_or_remove_health(true);
-      }
-      else
-      {}
-    }
-    else if(angryFace = true)
-    {
-      nPoint += 10;
-    }
-  }
+  lcd.setCursor(11,1);
+  lcd.print("NP:");
+  lcd.setCursor(14,1);
+  lcd.print(String(nPoint));
 }
+
+void game_over()
+{
+  lcd.clear();
+  lcd.setCursor(1,0);
+  lcd.print("GAME");
+  lcd.setCursor(1,1);
+  lcd.print("OVER");
+  delay(2000);
+  lcd.clear();
+  endGame = millis();
+  double gameTime = (endGame - startGame) / 1000;
+  lcd.setCursor(1,0);
+  lcd.print("Time:" + String(gameTime) + " sec");
+  lcd.setCursor(1,1);
+  lcd.print("Points:" + String(nPoint));
+}
+
 
